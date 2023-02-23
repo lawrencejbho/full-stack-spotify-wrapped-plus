e20f216a55810be99a5332dcc427ef0d560a73fb
Lawrence Ho <lawrence.jb.ho@gmail.com>
2023-02-22 17:20:51 -0800

    added refresh token, spotify search and play

--- client/src/Dashboard.jsx
+++ client/src/Dashboard.jsx
@@ -1,5 +1,79 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import useAuth from "./useAuth.jsx";
+import TrackSearchResults from "./TrackSearchResults.jsx";
+import Player from "./Player.jsx";
+import { Container, Form } from "react-bootstrap";
+import SpotifyWebApi from "spotify-web-api-node";
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: "501daf7d1dfb43a291ccc64c91c8a4c8",
+});
 
 export default function Dashboard({ code }) {
-  return <div>{code}</div>;
+  const accessToken = useAuth(code);
+  const [search, setSearch] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const [playingTrack, setPlayingTrack] = useState();
+
+  function chooseTrack(track) {
+    setPlayingTrack(track);
+    setSearch("");
+  }
+
+  useEffect(() => {
+    if (!accessToken) return;
+    spotifyApi.setAccessToken(accessToken);
+  }, [accessToken]);
+
+  useEffect(() => {
+    if (!search) return setSearchResults([]);
+    if (!accessToken) return;
+
+    let cancel = false; // if a new request is made then we want to cancel the original request
+    spotifyApi.searchTracks(search).then((res) => {
+      if (cancel) return;
+      setSearchResults(
+        res.body.tracks.items.map((track) => {
+          const smallestAlbumImage = track.album.images.reduce(
+            (smallest, image) => {
+              if (image.height < smallest.height) return image;
+              return smallest;
+            },
+            track.album.images[0]
+          );
+          return {
+            artist: track.artists[0].name,
+            title: track.name,
+            uri: track.uri,
+            albumUrl: smallestAlbumImage.url,
+          };
+        })
+      );
+    });
+
+    return () => (cancel = true);
+  }, [search, accessToken]);
+
+  return (
+    <Container className="d-flex flex-column py-2" style={{ height: "100vh" }}>
+      <Form.Control
+        type="search"
+        placeholder="Search Songs/Artists"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <div className="flex-grow-1 my-2" style={{ overflowY: "auto" }}>
+        {searchResults.map((track) => (
+          <TrackSearchResults
+            track={track}
+            key={track.uri}
+            chooseTrack={chooseTrack}
+          />
+        ))}
+      </div>
+      <div>
+        <Player accessToken={accessToken} trackUri={playingTrack?.uri} />
+      </div>
+    </Container>
+  );
 }
--- server/server.js
+++ server/server.js
@@ -1,6 +1,30 @@
 const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const SpotifyWebApi = require("spotify-web-api-node");
+
 const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post("/refresh", (req, res) => {
+  const refreshToken = req.body.refreshToken;
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: "http://localhost:5173",
+    clientId: "501daf7d1dfb43a291ccc64c91c8a4c8",
+    clientSecret: "9aa707866a144a66955d05e169c16214",
+    refreshToken,
+  });
+
+  spotifyApi
+    .refreshAccessToken()
+    .then((data) => {
+      console.log(data.body);
+    })
+    .catch(() => {
+      res.sendStatus(400);
+    });
+});
 
 app.post("/login", (req, res) => {
   const code = req.body.code;
@@ -19,8 +43,10 @@ app.post("/login", (req, res) => {
         expiresIn: data.body.expires_in_in,
       });
     })
-    .catch(() => {
-      console.log("error");
+    .catch((error) => {
+      console.log(error);
       res.sendStatus(400);
     });
 });
+
+app.listen(3001);
