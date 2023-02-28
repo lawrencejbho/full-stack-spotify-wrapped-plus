df3dd7c4585d11a552d03ef58c8b00e384d30955
Lawrence Ho <lawrence.jb.ho@gmail.com>
2023-02-28 15:55:49 -0800

    modified port and secure option on backend

--- client/vite.config.js
+++ client/vite.config.js
@@ -12,13 +12,11 @@ export default defineConfig({
   },
   server: {
     host: true,
-    port: 5173,
     proxy: {
       "/api": {
         // target: "http://localhost:3001",
-        target: "http://www.wrappedplus.com",
+        target: "https://www.wrappedplus.com",
         changeOrigin: true,
-        secure: false,
       },
     },
   },
